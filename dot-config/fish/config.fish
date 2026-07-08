b761af2f529753f9eedb7c50553907010b28f15b
Julien A Bitang <julienabitang@gmail.com>
2026-07-08 18:28:02 -0400

    cachyos configs

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,12 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-source ~/.config/fish/secrets.fish
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_PICTURES_DIR "$HOME/Pictures"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x MANPAGER "nvim --clean +Man!"
-    set -x HYPRSHOT_DIR "$XDG_PICTURES_DIR/screenshots"
     set -x EDITOR "/usr/bin/nvim"
     set -x VISUAL "/usr/bin/nvim"
 
@@ -15,5 +15,4 @@ if status is-interactive
     abbr -a gs git status
 
     abbr -a sail composer exec sail
-    fastfetch
 end
